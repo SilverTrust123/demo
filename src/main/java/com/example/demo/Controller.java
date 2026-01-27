629f85df3e87e6715373be65d137b0bf380ce536
SilverTrust123 <forschool819@gmail.com>
2026-01-27 15:42:27 +0800

    存檔

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "*")
 @RestController
