4cdcdb911257b9656e5421b9907c463f2bdb1d96
SilverTrust123 <forschool819@gmail.com>
2026-02-16 14:38:31 +0800

    seperate controller into little peices (all finish)

--- src/main/java/com/example/demo/controller/ControllerData.java
+++ src/main/java/com/example/demo/controller/ControllerData.java
@@ -5,11 +5,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.ServiceData;
 import org.slf4j.Logger;
 
+@RestController
+@RequestMapping("/data")
 public class ControllerData {
     private static final Logger log = LoggerFactory.getLogger(ControllerData.class);
 
