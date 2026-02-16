9e2bc789296249939fc60b5e704e9384f8e34e6a
SilverTrust123 <forschool819@gmail.com>
2026-02-16 14:19:41 +0800

    seperate controller into little peices (contoller temp n humi)

---
+++ src/main/java/com/example/demo/controller/ControllerCircuit.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.sensor.SensorDataCircuit;
+import com.example.demo.service.ServiceCircuit;
+
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ControllerCircuit {
+    @Autowired
+    private ServiceCircuit serviceCircuit;
+    private static final Logger log = LoggerFactory.getLogger(ControllerCircuit.class);
+
+    @PostMapping("/CircuitData")
+    public String receiveCircuitData(@RequestBody SensorDataCircuit data) {
+        log.info("Received and transfer circuit data");
+        return serviceCircuit.receiveCircuitData(data);
+    }
+
+    // 船空的回去就是找不到東西 或是太舊了
+    @GetMapping("/CircuitData/{deviceId}")
+    public SensorDataCircuit getCircuitData(@PathVariable String deviceId) {
+        log.info("Received and transfer request for circuit data of device");
+        return serviceCircuit.getCircuitData(deviceId);
+    }
+
+    // 這個本來最多就一個 如果船空的回去就是太舊了
+    @GetMapping("/CircuitData")
+    public Collection<SensorDataCircuit> getAllCircuitData() {
+        log.info("Received and transfer request for all circuit data");
+        return serviceCircuit.getAllCircuitData();
+    }
+}
---
+++ src/main/java/com/example/demo/controller/ControllerTemparatureAndHumidity.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.util.Collection;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.sensor.SensorDataTemperatureAndHumidity;
+import com.example.demo.service.ServiceTemparatureAndHumidity;
+import org.slf4j.Logger;
+
+@RestController
+@RequestMapping("/temperatureAndHumidity")
+public class ControllerTemparatureAndHumidity {
+    private static final Logger log = LoggerFactory.getLogger(ControllerTemparatureAndHumidity.class);
+
+    @Autowired
+    private ServiceTemparatureAndHumidity serviceTemparatureAndHumidity;
+
+    @PostMapping("/TemparatureAndHumidityData")
+    public String receiveTemparatureAndHumidityData(@RequestBody SensorDataTemperatureAndHumidity data) {
+        log.info("Received and transfertemperature and humidity data");
+        return serviceTemparatureAndHumidity.receiveTemparatureAndHumidityData(data);
+    }
+
+    // 船空的回去就是找不到東西
+    @GetMapping("/TemparatureAndHumidityData/{deviceId}")
+    public SensorDataTemperatureAndHumidity getTemparatureAndHumidityData(@PathVariable String deviceId) {
+        log.info("Received and transfer request for temperature and humidity data of device");
+        return serviceTemparatureAndHumidity.getTemparatureAndHumidityData(deviceId);
+    }
+
+    // 打掉太舊的資料 然後回傳剩下的 可能會只剩一個 等一下要跟董事長說一下
+    @GetMapping("/TemparatureAndHumidityData")
+    public Collection<SensorDataTemperatureAndHumidity> getAllTemparatureAndHumidityData() {
+        log.info("Received and transfer request for all temperature and humidity data");
+        return serviceTemparatureAndHumidity.getAllTemparatureAndHumidityData();
+    }
+}
