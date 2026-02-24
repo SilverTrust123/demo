373a0ad85aae32ec042c02688e80c4fd59acc11e
SilverTrust123 <forschool819@gmail.com>
2026-02-24 09:42:33 +0800

    add swagger-ui

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -12,7 +12,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 @RestController
 public class Controller {
     private static final Logger log = LoggerFactory.getLogger(Controller.class);
