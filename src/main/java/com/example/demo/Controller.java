f2ed9798873850e8999113742f93270fa23fd126
SilverTrust123 <forschool819@gmail.com>
2026-02-26 18:14:48 +0800

    continues add full log(db version)

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.service.ServiceLog;
 
 import java.io.IOException;
 import java.nio.file.Files;
