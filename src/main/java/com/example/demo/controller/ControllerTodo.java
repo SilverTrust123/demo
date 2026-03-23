d0c3034a427269ecab1f8b2d02cd91e7de70b94a
SilverTrust123 <forschool819@gmail.com>
2026-03-23 18:17:18 +0800

    update

--- src/main/java/com/example/demo/controller/ControllerTodo.java
+++ src/main/java/com/example/demo/controller/ControllerTodo.java
@@ -5,14 +5,12 @@ import java.util.concurrent.CompletableFuture;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.priorityQueueTask.QueueService;
 import com.example.demo.DTO.requestDTO.RequestTodoDTO;
-import com.example.demo.DTO.requestDTO.history.*;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 
