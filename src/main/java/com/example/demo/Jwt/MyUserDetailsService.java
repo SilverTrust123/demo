bbfbe14409ce73ae25d14888ca68823217a42959
SilverTrust123 <forschool819@gmail.com>
2026-06-12 14:34:43 +0800

    qs

--- src/main/java/com/example/demo/Jwt/MyUserDetailsService.java
+++ src/main/java/com/example/demo/Jwt/MyUserDetailsService.java
@@ -19,15 +19,13 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 用 Optional 避免 NullPointerException
+
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
-        // 如果你有角色可以改成 List<GrantedAuthority> authorities = ...
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                new ArrayList<>() // 空權限列表
-        );
+                new ArrayList<>());
     }
 }
