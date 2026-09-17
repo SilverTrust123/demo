be30eb09c8d1f3ccfea0059b2c9e29631570ed13
SilverTrust123 <forschool819@gmail.com>
2026-09-17 10:20:21 +0800

    qs

--- src/main/java/com/example/demo/service/Scheduled/ServiceHistoryArchive.java
+++ src/main/java/com/example/demo/service/Scheduled/ServiceHistoryArchive.java
@@ -11,13 +11,13 @@ import com.example.demo.service.ServiceAirQuality;
 import com.example.demo.service.ServiceCircuit;
 import com.example.demo.service.*;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collection;
+import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 @Service
